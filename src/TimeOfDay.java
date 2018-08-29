c250cc814c43089b15b5216a178db97fd75e22de
Austin <austin.thornton.2019@student.lhschools.org>
2018-08-29 14:19:08 -0400

    Added some time , hours , minutes, in progress

--- src/TimeOfDay.java
+++ src/TimeOfDay.java
@@ -1,2 +1,56 @@
 public class TimeOfDay {
+
+
+    private int mhour;
+    private int mMinutes;
+    private int hourcheck;
+    private int minutecheck;
+
+    public TimeOfDay() {
+        this(0,0);
+    }
+
+
+    public int getHourcheck(int pHour) {
+        return hourcheck;
+       if (23 < pHour) {
+           System.out.println("That number is to large");
+
+       if (23 > pHour) {
+           System.out.println();
+
+       }
+
+       if (59 < mMinutes) {
+
+           
+       }
+    }
+
+
+
+    public int getMinutecheck() {
+        return minutecheck;
+    }
+
+
+
+    public TimeOfDay(int pHour) {
+        this( pHour, 0);
+    }
+
+    public TimeOfDay(int phour, int pMinutes) {
+        this.mhour = phour;
+        this.mMinutes = pMinutes;
+    }
+
+
+    public int getMhour() {
+        return mhour;
+    }
+
+    public int getmMinutes() {
+        return mMinutes;
+    }
+
 }
