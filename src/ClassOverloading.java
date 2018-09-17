dc6b1601f4fca60cbc9f90dd3c60d2c58e129d8e
Austin <austin.thornton.2019@student.lhschools.org>
2018-09-17 13:28:58 -0400

    added in year and month

--- src/ClassOverloading.java
+++ src/ClassOverloading.java
@@ -6,6 +6,9 @@ public class ClassOverloading {
     private TimeOfDay then;
     int hourDifference;
     int minDifference;
+    int monthDifference;
+    int dayDifference;
+
 
     public static void main(String[] args) {
         ClassOverloading classoverloading = new ClassOverloading();
@@ -17,17 +20,25 @@ public class ClassOverloading {
     public void init() {
 
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("Enter a time with an hour and minutes");
-        now = setTimeOfDay(keyboard, now);
-        System.out.println("\nSet your second set of time of hours and minutes\n");
-        then = setTimeOfDay(keyboard, then);
-        System.out.println("First Set of hours & minutes = "+now.getMhour() + ":" + now.getmMinutes());
-        System.out.println("Second Set of hours & minutes = "+then.getMhour() + ":" + then.getmMinutes());
 
-        if(time(now,then)) {
-            System.out.println("\n The time difference is " + hourDifference + ":" + minDifference);
-        }
-        else{
+        System.out.println("Enter a Month, then hours and minutes");
+        now = setTimeOfDay(keyboard, now);
+        System.out.println("\nSet your second: Enter a Month, then hours and minutes\n");
+        then = setTimeOfDay(keyboard, then);
+        System.out.println("First Set of Month & hours & minutes = " + now.getmMonth() + "/" +
+                now.getmDay() + "/" +
+                now.getMhour() + ":" +
+                now.getmMinutes());
+
+        System.out.println("Second Set of hours & minutes = " +
+                then.getmMonth() + "/" +
+                now.getmDay() + "/" +
+                then.getMhour() + ":" +
+                then.getmMinutes());
+
+        if (time(now, then)) {
+            System.out.println("\n The time difference is " + monthDifference + "/" + dayDifference + "/" +hourDifference + ":" + minDifference);
+        } else {
             System.out.println("You gave the same times");
         }
 
@@ -37,19 +48,21 @@ public class ClassOverloading {
     public TimeOfDay setTimeOfDay(Scanner pkeyboard, TimeOfDay now) {
         boolean valid = false;
         while (!valid) {
+            System.out.println("Enter In the month #");
+            int pMonth = pkeyboard.nextInt();
+            System.out.println("Enter In the day of the month");
+            int pDay = pkeyboard.nextInt();
             System.out.println("Enter The Hour");
             int pHours = pkeyboard.nextInt();
             System.out.println("Enter The Minutes");
             int pMinutes = pkeyboard.nextInt();
-            if (validhours(pHours) && validminutes(pMinutes)) {
-                now = new TimeOfDay(pHours, pMinutes);
+            if (validhours(pHours) && validminutes(pMinutes) && validmonth((pMonth)) && validDay(pDay)) {
+                now = new TimeOfDay(pMonth, pDay, pHours, pMinutes);
                 valid = true;
                 return now;
             } else {
-                System.out.println("You have entered an invalid time:"
-                        + pHours + ":" + pMinutes + " please enter in a valid time");
+                System.out.println("You have entered an invalid time:" + pMonth + "/" + pDay + "/" + pHours + ":" + pMinutes + " please enter in a valid time");
             }
-
 
 
         }
@@ -80,18 +93,42 @@ public class ClassOverloading {
         }
     }
 
+    private boolean validmonth(int pMonth) {
+        if (pMonth > 0 && pMonth < 13) {
+            return true;
+        } else
+            return false;
+    }
+    private boolean validDay(int pDay) {
+        if (pDay > 0 && pDay < 31) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     private boolean time(TimeOfDay now, TimeOfDay then) {
 
-        if (now.getMhour() - then.getMhour() != 0 || now.getmMinutes() - then.getmMinutes() != 0) {
+        if (
+                now.getmDay() - then.getmDay() != 0 ||
+                now.getmMonth() - then.getmMonth() != 0 ||
+                now.getMhour() - then.getMhour() != 0 ||
+                now.getmMinutes() - then.getmMinutes() != 0) {
+
+
+            monthDifference = (Math.abs(now.getmMonth()-then.getmMonth()));
 
             hourDifference = (Math.abs(now.getMhour() - then.getMhour()));
 
             minDifference = (Math.abs(now.getmMinutes() - then.getmMinutes()));
+
+            dayDifference = (Math.abs(now.getmDay()-then.getmDay()));
+
             return true;
 
         } else {
 
-                System.out.println("This should not be happening");
+                System.out.println(">> Same Times");
                 return false;
         }
 
--- src/TimeOfDay.java
+++ src/TimeOfDay.java
@@ -1,14 +1,19 @@
 import com.sun.xml.internal.txw2.IllegalAnnotationException;
 
-import java.lang.reflect.Array;
-
 public class TimeOfDay {
 
-
+    private int myear;
+    private int mDay;
+    private int mMonth;
     private int mhour;
     private int mMinutes;
 
 
+    public int getmYear() { return myear;}
+
+    public int getmMonth() {
+        return mMonth;
+    }
 
     public int getMhour() {
         return mhour;
@@ -18,8 +23,12 @@ public class TimeOfDay {
         return mMinutes;
     }
 
+    public int getmDay() {
+        return mDay;
+    }
+
     public TimeOfDay() {
-        this(0, 0);
+        this(0,0,0,0, 0);
 
     }
 
@@ -28,16 +37,34 @@ public class TimeOfDay {
     }
 
 
-    public TimeOfDay(int pHour, int pMinutes) {
-        if (pHour > 23 || pHour < 0 || pMinutes > 59 || pMinutes < 0)
+    public TimeOfDay(int pYear, int pMonth, int pDay, int pHour, int pMinutes) {
+        if (pMonth > 12 || pMonth < 0 || pHour > 23 || pHour < 0 || pMinutes > 59 || pMinutes < 0)
             throw new IllegalAnnotationException(
                     "Bad input of time: Hour:" + pHour + " Minutes " + pMinutes
                             + " " + TimeOfDay.class.getSimpleName());
 
-
+        this.myear = pYear;
+        this.mDay = pDay;
         this.mhour = pHour;
         this.mMinutes = pMinutes;
+        this.mMonth = pMonth;
 
+
+        if (pYear > 2019 || pYear < 0) {
+            throw new IllegalArgumentException("Bad Year data input:" + pYear);
+        }
+        else
+        {
+            this.myear = pYear;
+        }
+
+        if (pMonth > 12 || pMonth < 0) {
+            throw new IllegalArgumentException("Bad data input for month" + pMonth);
+        } else {
+            this.mMonth = pMonth;
+
+
+        }
         if (pHour > 23 || pHour < 0) {
             throw new IllegalArgumentException("Bad data Input: Hour " + pHour);
 
@@ -54,9 +81,18 @@ public class TimeOfDay {
             this.mMinutes = pMinutes;
         }
 
+        if (pDay > 31 || pDay < 0) {
+            throw new IllegalArgumentException("Bad data input" + pDay);
+        }
+        else {
+            this.mDay = pDay;
+
+            }
+        }
+
 
 
     }
-}
+
 
 
